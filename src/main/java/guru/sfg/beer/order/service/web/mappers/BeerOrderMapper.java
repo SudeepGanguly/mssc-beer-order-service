de4a10df4da72bd238e7ad3f35a5b0d4d65b27e6
SSARIpa3 <swati.0491@gmail.com>
2021-05-09 09:02:15 +0530

    Update the POM to inherit the dependencies from BOM

--- src/main/java/guru/sfg/beer/order/service/web/mappers/BeerOrderMapper.java
+++ src/main/java/guru/sfg/beer/order/service/web/mappers/BeerOrderMapper.java
@@ -24,7 +24,6 @@ import org.mapstruct.Mapping;
 
 @Mapper(uses = {DateMapper.class, BeerOrderLineMapper.class})
 public interface BeerOrderMapper {
-
     @Mapping(target = "customerId", source = "customer.id")
     BeerOrderDto beerOrderToDto(BeerOrder beerOrder);
 

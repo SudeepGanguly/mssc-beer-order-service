dbbf39d0fff73ea9b9b3f6c5fc9cea442b9ec9b0
SSARIpa3 <swati.0491@gmail.com>
2021-05-09 12:19:25 +0530

    Correcting errors after updating to BOM

--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-05-09T08:59:31+0530",
+    date = "2021-05-09T10:35:54+0530",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 13.0.1 (Oracle Corporation)"
 )
 @Component

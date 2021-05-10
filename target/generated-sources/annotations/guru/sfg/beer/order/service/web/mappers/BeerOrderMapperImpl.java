e404d07cc59ba12accaf925c0974812512857405
SSARIpa3 <swati.0491@gmail.com>
2021-05-10 10:49:50 +0530

    Configuring spring profile for local MySQL.

--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderMapperImpl.java
@@ -19,7 +19,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-05-09T10:35:53+0530",
+    date = "2021-05-10T10:28:36+0530",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 13.0.1 (Oracle Corporation)"
 )
 @Component

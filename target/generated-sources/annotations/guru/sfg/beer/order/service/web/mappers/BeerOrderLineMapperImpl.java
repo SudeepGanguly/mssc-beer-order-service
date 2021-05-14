cdb8239a3b3e8d7b5b502b13fdea7f5617d6a001
SSARIpa3 <swati.0491@gmail.com>
2021-05-14 08:34:22 +0530

    Adding Enumerations for Spring State Machine

---
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
@@ -0,0 +1,15 @@
+package guru.sfg.beer.order.service.web.mappers;
+
+import javax.annotation.processing.Generated;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2021-05-11T18:23:32+0530",
+    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 13.0.1 (Oracle Corporation)"
+)
+@Component
+@Primary
+public class BeerOrderLineMapperImpl extends BeerOrderLineDecorator implements BeerOrderLineMapper {
+}

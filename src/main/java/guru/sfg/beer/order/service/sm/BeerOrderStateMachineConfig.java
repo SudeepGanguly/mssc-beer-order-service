2f66a52ea450cc5dc9c093349dfe0c7b772101f2
SSARIpa3 <swati.0491@gmail.com>
2021-05-14 09:20:48 +0530

    Step 4 : Configure all state machine transitions for validation transition

--- src/main/java/guru/sfg/beer/order/service/sm/BeerOrderStateMachineConfig.java
+++ src/main/java/guru/sfg/beer/order/service/sm/BeerOrderStateMachineConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
 
 import java.util.EnumSet;
 
@@ -22,5 +23,43 @@ public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<B
                 .end(BeerOrderStatusEnum.DELIVERED)
                 .end(BeerOrderStatusEnum.DELIVERY_EXCEPTION)
                 .end(BeerOrderStatusEnum.PICKED_UP);
+    }
+
+    @Override
+    public void configure(StateMachineTransitionConfigurer<BeerOrderStatusEnum, BeerOrderEvents> transitions) throws Exception {
+        transitions
+           //Validation Transitions
+                .withExternal()
+                .source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.NEW)
+                .event(BeerOrderEvents.VALIDATE_ORDER)
+                //todo add a validation action
+          .and()
+                .withExternal()
+                .source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.VALIDATED)
+                .event(BeerOrderEvents.VALIDATION_PASSED)
+          .and()
+                .withExternal()
+                .source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.VALIDATION_EXCEPTION)
+                .event(BeerOrderEvents.VALIDATION_FAILED)
+
+          //Allocation Transitions
+          .and()
+                .withExternal()
+                .source(BeerOrderStatusEnum.VALIDATED).target(BeerOrderStatusEnum.ALLOCATED)
+                .event(BeerOrderEvents.ALLOCATION_SUCESS)
+          .and()
+                .withExternal()
+                .source(BeerOrderStatusEnum.VALIDATED).target(BeerOrderStatusEnum.ALLOCATION_EXCEPTION)
+                .event(BeerOrderEvents.ALLOCATION_FAILED)
+          .and()
+                .withExternal()
+                .source(BeerOrderStatusEnum.VALIDATED).target(BeerOrderStatusEnum.VALIDATED)
+                .event(BeerOrderEvents.ALLOCATION_NO_INVENTORY)
+
+           //Delivery Transitions
+           .and()
+                .withExternal()
+                .source(BeerOrderStatusEnum.ALLOCATED).target(BeerOrderStatusEnum.PICKED_UP)
+                .event(BeerOrderEvents.BEERORDER_PICKED_UP);
     }
 }
